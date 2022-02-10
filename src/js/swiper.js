663d1ffe8ef590abebba32ef9e73ef21ea8625a0
Denis <deniska01.06.01@gmail.com>
2022-02-10 21:27:26 +0300

    end first page, start mobile version

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,12 @@
 var swiper = new Swiper(".hero-swiper", {
+    effect: 'fade',
     pagination: {
       el: ".swiper-pagination",
       bulletClass: "swiper__bullet",
       bulletActiveClass: "swiper__bullet-active",
     },
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
   });
